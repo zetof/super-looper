bbb033481cfdfcd64e8bfd7ac81133e92aa1619d
Christophe Bury <christophe.bury@netaxis.be>
2021-02-04 12:29:56 +0100

    Multiple keys handling in main loop

--- main.py
+++ main.py
@@ -29,15 +29,11 @@ lpd8.set_knob_value(Programs.PGM_4, Knobs.KNOB_1, DEFAULT_BPM)
 lpd8.set_not_sticky_knob(Programs.PGM_4, Knobs.KNOB_1)
 lpd8.set_pad_mode(Programs.PGM_4, Pads.PAD_5, Pad.SWITCH_MODE + Pad.BLINK_MODE)
 lpd8.set_pad_switch_state(Programs.PGM_4, Pads.PAD_5, Pad.ON)
-lpd8.subscribe(metronome, metronome.set_bpm, Programs.PGM_4, LPD8.CTRL,
-               Knobs.KNOB_1)
-lpd8.subscribe(w_bpm, w_bpm.bpm_update, Programs.PGM_4, LPD8.CTRL,
-               Knobs.KNOB_1)
-lpd8.subscribe(metronome, metronome.pause, Programs.PGM_4, LPD8.NOTE_ON,
-               Pads.PAD_5)
+lpd8.subscribe(metronome, metronome.set_bpm, Programs.PGM_4, LPD8.CTRL, Knobs.KNOB_1)
+lpd8.subscribe(w_bpm, w_bpm.bpm_update, Programs.PGM_4, LPD8.CTRL, Knobs.KNOB_1)
+lpd8.subscribe(metronome, metronome.pause, Programs.PGM_4, LPD8.NOTE_ON, Pads.PAD_5)
 metronome.subscribe(MetronomeEvents.BEAT, lpd8, lpd8.pad_update)
-metronome.subscribe(MetronomeEvents.TICK, w_metronome,
-                    w_metronome.tick_update, with_data=True)
+metronome.subscribe(MetronomeEvents.TICK, w_metronome, w_metronome.tick_update, with_data=True)
 lpd8.pad_update()
 lpd8.start()
 osc.start()
@@ -47,8 +43,11 @@ metronome.start()
 running = True
 while running:
     screen.refresh()
-    if screen.key_pressed() == ord('r'):
-        metronome.set_position(10, 0, 0)
+    action = screen.key_pressed()
+    if action == ord('q'):
+        running = False
+    elif action == ord('r'):
+        metronome.set_position()
     sleep(.01)
 
 # We tidy up things and kill all processes
